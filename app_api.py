d0eb6b1e1b0ba864051a81dde77339404be16dd2
candiwestin <candiwestin@gmail.com>
2026-03-14 17:17:17 -0700

    Fix CORS configuration for GitHub Pages

--- app_api.py
+++ app_api.py
@@ -32,7 +32,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
     allow_credentials=False,
-    allow_methods=["*"],
+    allow_methods=["GET", "POST", "HEAD", "OPTIONS"],
     allow_headers=["*"],
 )
 
